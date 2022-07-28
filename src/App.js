c3f92c8f546bb058a7554d6ecdf6e19dc96cd96e
Anthony Chen <anthonychen33@gmail.com>
2022-07-28 17:04:51 -0400

    Starting files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {useState, useEffect} from 'react';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div> Hello from App!</div>
+    </div>
+  );
+};
+
+export default App;
