390c7deda09baa73aa2cc293446d1f271d003e16
Anthony Chen <anthonychen33@gmail.com>
2022-08-05 16:12:34 -0400

    Able to get back data from a child

---
+++ src/data/items.js
@@ -0,0 +1,32 @@
+import greenBubbleGunImage from '../images/green-bubble-gun.jpeg';
+import blueBubbleGunImage from '../images/blue-bubble-gun.jpeg';
+import purpleBubbleGunImage from '../images/purple-bubble-gun.jpg';
+
+const items = [
+  {
+    id: 0,
+    src: greenBubbleGunImage,
+    name: 'greenBubbleGun',
+    price: '$7.99',
+    category: 'Pistol',
+    quantity: 0,
+  },
+  {
+    id: 1,
+    src: blueBubbleGunImage,
+    name: 'blueBubbleGun',
+    price: '$8.99',
+    category: 'Pistol',
+    quantity: 0,
+  },
+  {
+    id: 2,
+    src: purpleBubbleGunImage,
+    name: 'purpleBubbleGun',
+    price: '$9.99',
+    category: 'Pistol',
+    quantity: 0,
+  },
+];
+
+export default items;
