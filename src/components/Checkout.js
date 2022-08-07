86740d3c08f15d36a726e86d809a6cfde09ab451
Anthony Chen <anthonychen33@gmail.com>
2022-08-07 19:11:42 -0400

    Working on layout of Checkout page

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import {useState, useEffect} from 'react';
@@ -5,6 +6,14 @@ import NavBar from './NavBar';
 import '../styles/Checkout.css';
 
 const Checkout = ({cart, totalCost, getSinglePrice}) => {
+  const renderTotalCost = () => {
+    if ( totalCost === 0 ) {
+      return <div></div>;
+    } else {
+      return <div className="total-cost-element">Total Cost: ${totalCost}</div>;
+    }
+  };
+
   const showCartItems = () => {
     const cartArr = [...cart];
 
@@ -13,7 +22,7 @@ const Checkout = ({cart, totalCost, getSinglePrice}) => {
       return <li key={cartElement.id} className="cart-item">
         <img src={cartElement.src} alt={'bubbleGun'}/>
         <div>Quantity: {cartElement.quantity}</div>
-        <div>Quantity: {cartElement.quantity}</div>
+        <div>Price: ${getSinglePrice(cartElement.price) * cartElement.quantity}</div>
       </li>;
     });
 
@@ -31,9 +40,10 @@ const Checkout = ({cart, totalCost, getSinglePrice}) => {
   };
 
   return (
-    <div>
-      <h1>You have {numberOfItems()} item in your cart!</h1>
+    <div className="checkout-container">
+      <h1 className="header-cart-info">You have {numberOfItems()} item in your cart!</h1>
       <ul>{showCartItems()}</ul>
+      {renderTotalCost()}
     </div>
   );
 };
