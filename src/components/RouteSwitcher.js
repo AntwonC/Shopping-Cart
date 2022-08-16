2ccea2bc732a1dfab5c882259e2598891320c6ed
Anthony Chen <anthonychen33@gmail.com>
2022-08-16 04:22:16 -0400

    Updates

--- src/components/RouteSwitcher.js
+++ src/components/RouteSwitcher.js
@@ -72,10 +72,10 @@ const RouteSwitcher = () => {
   };
 
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <NavBar cartNotif={cartAmount}/>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/Home" element={<App />} />
         <Route path="/About" element={<About />} />
         <Route path="/Store"
           element={<Store
